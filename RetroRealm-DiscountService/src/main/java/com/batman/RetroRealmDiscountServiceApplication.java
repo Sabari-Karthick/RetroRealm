8bdc2967c20786f87df921a7cd84efc068f186df
SK <sabariks2003@gmail.com>
2024-06-18 23:49:44 +0530

    Discount service created

---
+++ RetroRealm-DiscountService/src/main/java/com/batman/RetroRealmDiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.batman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroRealmDiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroRealmDiscountServiceApplication.class, args);
+	}
+
+}
