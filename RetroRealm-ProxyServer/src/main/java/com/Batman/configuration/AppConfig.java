3c909120389d5ed1b38fa089605672f402c96033
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-07-30 12:45:47 +0530

    Admin Server Port changed

--- RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/AppConfig.java
+++ RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/AppConfig.java
@@ -3,17 +3,19 @@ package com.Batman.configuration;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
   
-	@Bean
-	RedisRateLimiter gameServiceRateLimiter() {
+	@Bean(name = "gameLimiter")
+	@Primary
+	RedisRateLimiter gameServiceRateLimit() {
 		return new RedisRateLimiter(10, 20, 1);
 	}
 	
 	
-	@Bean
+	@Bean(name = "orderLimiter")
 	RedisRateLimiter orderServiceRateLimiter() {
 		return new RedisRateLimiter(5, 20, 1);
 	}
