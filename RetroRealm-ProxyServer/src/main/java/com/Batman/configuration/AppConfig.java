2f1204dcb651aedcdc54f0f8c74fa7f013c78a30
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-07-30 16:35:03 +0530

    Minor Fixes

--- RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/AppConfig.java
+++ RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/AppConfig.java
@@ -3,22 +3,21 @@ package com.Batman.configuration;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
-public class AppConfig {
-  
-	@Bean(name = "gameLimiter")
-	@Primary
-	RedisRateLimiter gameServiceRateLimit() {
+public class AppConfig  {
+   
+
+	@Bean
+	RedisRateLimiter rateLimter() {
 		return new RedisRateLimiter(10, 20, 1);
 	}
 	
-	
-	@Bean(name = "orderLimiter")
-	RedisRateLimiter orderServiceRateLimiter() {
-		return new RedisRateLimiter(5, 20, 1);
-	}
-	
+//	
+//	@Bean
+//	RedisRateLimiter orderServiceRateLimiter() {
+//		return new RedisRateLimiter(5, 20, 1);
+//	}
+//	
 	
 }
