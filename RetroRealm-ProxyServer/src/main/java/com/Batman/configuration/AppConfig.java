903161f771c503f1050bb754eb2940f5e0c7305b
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-07-30 12:03:36 +0530

    Port Update and Ratelimting for game service update in gateway

---
+++ RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/AppConfig.java
@@ -0,0 +1,22 @@
+package com.Batman.configuration;
+
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+  
+	@Bean
+	RedisRateLimiter gameServiceRateLimiter() {
+		return new RedisRateLimiter(10, 20, 1);
+	}
+	
+	
+	@Bean
+	RedisRateLimiter orderServiceRateLimiter() {
+		return new RedisRateLimiter(5, 20, 1);
+	}
+	
+	
+}
---
+++ RetroRealm-ProxyServer/src/main/java/com/Batman/keyresolver/ProxyClientAddressKeyResolver.java
@@ -0,0 +1,33 @@
+package com.Batman.keyresolver;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Primary
+@Component(value = "proxyKeyResolver")
+@Slf4j
+public class ProxyClientAddressKeyResolver implements KeyResolver {
+	
+	
+	@Override
+	public Mono<String> resolve(ServerWebExchange exchange) {
+		log.info("Entering Proxy Key Resolver");
+		XForwardedRemoteAddressResolver addressResolver = XForwardedRemoteAddressResolver.maxTrustedIndex(2);
+		InetSocketAddress inetSocketAddress = addressResolver.resolve(exchange);
+		InetAddress inetAddress = inetSocketAddress.getAddress();
+		String hostAddress = inetAddress.getHostAddress();
+		log.info("Host Address {}",hostAddress);
+		log.info("Leaving Proxy Key Resolver");
+		return Mono.just(hostAddress);
+	}
+
+}
