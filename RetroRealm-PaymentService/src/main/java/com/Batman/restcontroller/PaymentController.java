73a000e4fce26af1d918c948b9ed46edc9d0df6d
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-08-07 16:19:07 +0530

    Payment Service Added and Resilence depedency change

--- RetroRealm-PaymentService/src/main/java/com/Batman/restcontroller/PaymentController.java
+++ RetroRealm-PaymentService/src/main/java/com/Batman/restcontroller/PaymentController.java
@@ -1,19 +1,26 @@
 package com.Batman.restcontroller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Batman.dto.PaymentRequest;
+import com.Batman.service.IPaymentService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/payment")
+@RequiredArgsConstructor
 public class PaymentController {
+	
+	private final IPaymentService paymentService;
 
 	@GetMapping("/pay")
-	String pay() {
-		return "Payed!!";
+	ResponseEntity<?> pay(@RequestBody PaymentRequest paymentRequest) {
+		return ResponseEntity.ok(paymentService.pay(paymentRequest.getAmount(), paymentRequest.getPaymentType()));
 	}
-	@GetMapping("/getPay")
-	String getPay() {
-		return "Bill";
-	}
+	
 }
