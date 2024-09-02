34e5b92b99238874bc7e34a944e5ffb18acf0359
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-09-02 18:00:28 +0530

    AppConfig Update

--- RetroRealm-GameService/src/main/java/com/Batman/configuration/AppConfig.java
+++ RetroRealm-GameService/src/main/java/com/Batman/configuration/AppConfig.java
@@ -5,16 +5,25 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import lombok.RequiredArgsConstructor;
-
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
+	
+	
 	@Bean
 	ModelMapper modelMapper() {
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return mapper;
 	}
+	
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
+    }
+    
 }
