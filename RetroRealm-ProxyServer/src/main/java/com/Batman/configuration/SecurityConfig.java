010cf983ef0b7f4f7e5242f1069f554b428a5f68
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-09-23 19:15:51 +0530

    Security Config Update

--- RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/SecurityConfig.java
+++ RetroRealm-ProxyServer/src/main/java/com/Batman/configuration/SecurityConfig.java
@@ -3,9 +3,15 @@ package com.Batman.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.Batman.security.jwt.JwtFilter;
+import com.Batman.security.oauth.CustomOauth2Service;
+import com.Batman.security.oauth.OAuth2SuccessHandler;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,16 +20,29 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+	
+	private final JwtFilter jwtFilter;
+	
+	private final CustomOauth2Service oauth2Service;
+	
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
 	@Bean
-	SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(request -> request
-//                        .pathMatchers("/eureka/**").permitAll()
-//                        .anyExchange().authenticated())
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                		.requestMatchers("/oauth2/**",  "/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .requestMatchers("/eureka/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(login -> login
+						.authorizationEndpoint(authorization -> authorization.baseUri("/oauth2/authorize"))
+						.userInfoEndpoint(userInfo -> userInfo.userService(oauth2Service))
+						.successHandler(oAuth2SuccessHandler))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 
