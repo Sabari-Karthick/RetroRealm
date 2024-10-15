695cde0cc133bab784ef78a01eabe9521535fb04
ssabarikarthick <ssabarikarthick@vanenburg.in>
2024-10-15 16:25:09 +0530

    Java Docs Added for Service Registry

--- RetroRealm-ServiceRegistry/src/main/java/com/Batman/RetroRealmServiceRegistryApplication.java
+++ RetroRealm-ServiceRegistry/src/main/java/com/Batman/RetroRealmServiceRegistryApplication.java
@@ -4,6 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main application class for the RetroRealm Eureka Service Registry.
+ *
+ * This application acts as the Eureka Server for service discovery,
+ * enabling microservices to register themselves and discover other
+ * services in the system.
+ * 
+ * <p>
+ * Annotations used:
+ * <ul>
+ *   <li>{@code @SpringBootApplication} - Marks this as a Spring Boot application.</li>
+ *   <li>{@code @EnableEurekaServer} - Enables the Eureka Server functionality.</li>
+ * </ul>
+ * </p>
+ *
+ * @author SK
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class RetroRealmServiceRegistryApplication {
